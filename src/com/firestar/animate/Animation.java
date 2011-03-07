9b5df9118ceb62a3d5120baa7df1c66704d62c46
Andrew Ardill <andrew.ardill@gmail.com>
2011-03-07 23:48:10 +1100

    Animation and Animator classes will group information for us. Keeping these as objects reduces the number of Maps needed, and also allows us to use the power of OO to design a better plugin!

---
+++ src/com/firestar/animate/Animation.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.firestar.animate;
+
+/**
+ * @author Cogito
+ *
+ */
+public class Animation {
+    private frameset frames;
+    private boolean playing;
+    private boolean repeat;
+    private boolean areaSet;
+}
---
+++ src/com/firestar/animate/Animator.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.firestar.animate;
+
+/**
+ * @author Cogito
+ *
+ */
+public class Animator {
+
+}
