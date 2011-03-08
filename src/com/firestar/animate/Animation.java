bb01e57414dbc9381d268229c935ecff2bae87a8
Andrew Ardill <andrew.ardill@gmail.com>
2011-03-09 01:51:12 +1100

    Extended the functionality of Animator and Animation.

--- src/com/firestar/animate/Animation.java
+++ src/com/firestar/animate/Animation.java
@@ -3,7 +3,16 @@
  */
 package com.firestar.animate;
 
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
 /**
+ * An animation of MineCraft blocks.
+ * Includes the frame set that describes each frame of the animation, 
+ * where the animation is located and some information about how it should be played.
+ * 
  * @author Cogito
  *
  */
@@ -12,4 +21,87 @@ public class Animation {
     private boolean playing;
     private boolean repeat;
     private boolean areaSet;
+    private Area area;
+    private Map<Location, Byte> lastFrameBytes;
+    private Map<Location, Material> lastFrameTypes;
+
+    public Animation() {
+        this(false);
+    }
+    public Animation(boolean repeat) {
+        super();
+        this.playing = false;
+        this.repeat  = repeat;
+        this.areaSet = false;
+    }
+    /**
+     * @return if the animation is playing
+     */
+    public final boolean isPlaying() {
+        return playing;
+    }
+    /**
+     * Start the animation.
+     */
+    public final void onStart() {
+        this.playing = true;
+    }
+    /**
+     * Stop the animation.
+     */
+    public final void onStop() {
+        this.playing = false;
+    }
+    /**
+     * @return if the animation is repeating
+     */
+    public final boolean isRepeat() {
+        return repeat;
+    }
+    /**
+     * @param repeat set if the animation should repeat
+     */
+    public final void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+    /**
+     * @return if the location of the animation is set
+     */
+    public final boolean isAreaSet() {
+        return areaSet;
+    }
+    /**
+     * @param area the area to set
+     */
+    public final void setArea(Area area) {
+        if(!areaSet) {
+            this.area = area;
+            this.areaSet = true;
+        }
+    }
+    /**
+     * @return the area
+     */
+    public final Area getArea() {
+        return area;
+    }
+    /**
+     * @return the frame set that contains all the frames for this animation/
+     */
+    public final frameset getFrames() {
+        return frames;
+    }
+    public void setLastFrameBytes(Map<Location, Byte> lastFrameBytes) {
+        this.lastFrameBytes = lastFrameBytes;
+    }
+    public Map<Location, Byte> getLastFrameBytes() {
+        return lastFrameBytes;
+    }
+    public void setLastFrameTypes(Map<Location, Material> lastFrameTypes) {
+        this.lastFrameTypes = lastFrameTypes;
+    }
+    public Map<Location, Material> getLastFrameTypes() {
+        return lastFrameTypes;
+    }
+
 }
--- src/com/firestar/animate/Animator.java
+++ src/com/firestar/animate/Animator.java
@@ -3,10 +3,73 @@
  */
 package com.firestar.animate;
 
+import org.bukkit.Location;
+
 /**
+ * An animator creates and edits animations.
+ * 
+ * They are able to specify where an animation should be created, and can add frames to an animations frameset.
  * @author Cogito
  *
  */
 public class Animator {
+    private Location loc1;
+    private Location loc2;
+    private Animation openAnimation;
+    
+    public Animator() {
+        super();
+    }
 
+    public boolean hasOpenAnimation() {
+        return openAnimation != null;
+    }
+
+    public Animation getOpenAnimation() {
+        return openAnimation;
+    }
+
+    public void closeAnimation() {
+        openAnimation = null;
+    }
+
+    public void openAnimation(Animation animation) {
+        if (hasOpenAnimation()) {
+            return;
+        } else {
+            openAnimation = animation;
+        }
+    }
+
+    /**
+     * @return the first location
+     */
+    public final Location getLoc1() {
+        return loc1;
+    }
+
+    /**
+     * @param location the first location
+     */
+    public final void setLoc1(Location location) {
+        this.loc1 = location;
+    }
+
+    /**
+     * @return the second location
+     */
+    public final Location getLoc2() {
+        return loc2;
+    }
+
+    /**
+     * @param location the second location
+     */
+    public final void setLoc2(Location location) {
+        this.loc2 = location;
+    }
+
+    public boolean locationsSet() {
+        return loc1 != null && loc2 != null;
+    }
 }
